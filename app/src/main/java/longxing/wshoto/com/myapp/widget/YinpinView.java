a2f7389d8653fdcd6dfe2a9d9bac9597f56efe9e
chenyi <2091320109@qq.com>
2018-01-27 17:58:56 +0800

    新增3.6.3.2音频条控件

---
+++ app/src/main/java/longxing/wshoto/com/myapp/widget/YinpinView.java
@@ -0,0 +1,70 @@
+package longxing.wshoto.com.myapp.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by user on 2018/1/27.
+ * 2091320109@qq.com
+ */
+
+public class YinpinView extends View {
+    private int mWidth;
+    private int mRectWidth;
+    private int mRectHeight;
+    private Paint mPaint;
+    private int mRectCount;
+
+    public YinpinView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        for (int i = 0; i < mRectCount; i++) {
+            double random = Math.random();
+            float currentHeight = (float) (mRectHeight * random);
+            int offset = 5;
+            canvas.drawRect(
+                    (float) (mWidth * 0.4 / 2 + mRectWidth * i + offset),
+                    currentHeight,
+                    (float) (mWidth * 0.4 / 2 + mRectWidth * (i + 1)),
+                    mRectHeight,
+                    mPaint);
+        }
+        postInvalidateDelayed(300);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = getWidth();
+        mRectHeight = getHeight();
+        mRectWidth = (int) (mWidth * 0.6 / mRectCount);
+        LinearGradient linearGradient = new LinearGradient(
+                0,
+                0,
+                mRectWidth,
+                mRectHeight,
+                Color.YELLOW,
+                Color.BLUE,
+                Shader.TileMode.CLAMP);
+        mPaint.setShader(linearGradient);
+    }
+
+    private void initView() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.FILL);
+        mRectCount = 12;
+    }
+}
